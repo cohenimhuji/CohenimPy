b69e1517e55cd08a7f44acee0e6e8a29bf39a5b4
cohenimhuji <nimrod.cohen1@mail.huji.ac.il>
2021-02-16 20:42:34 +0200

    Update ncPlot.py

--- CohenimPy/ncPlot.py
+++ CohenimPy/ncPlot.py
@@ -47,7 +47,7 @@ bbox2= (0.9, -0.12)
 # plt.style.use('seaborn') # pretty matplotlib plots
 
 def printVersion():
-    print('Cohenim Version:'0.016'])
+    print('Cohenim Version: 0.0162' )
     return
 
 def list_rotate(seq, n):
